42b3b33dcec6028d2491704f40de8b176df0ab73
7141-THAITQ-DT\admin <thaimeo0210@gmail.com>
2021-03-18 14:58:22 +0700

    fix bug

--- src/Components/ProductItem/index.js
+++ src/Components/ProductItem/index.js
@@ -18,7 +18,7 @@ export default class ProductItem extends Component {
         <Text style={defaultstyles.title}>{product.title}</Text>
         <Text style={defaultstyles.subtitle}>{product.number}</Text>
         <View style={defaultstyles.priceContainer}>
-          <Text style={defaultstyles.priceText}>{this.props.price}</Text>
+          <Text style={defaultstyles.priceText}>{product.price}</Text>
           <TouchableOpacity style={defaultstyles.plusButton}>
             <Icon name="plus" size={20} color={'white'} />
           </TouchableOpacity>
